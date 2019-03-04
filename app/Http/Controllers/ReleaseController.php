dfa11e6aab2cbcd8000f567f95141a9b244d7d3c
Bruno Barros <brunobarros2014@gmail.com>
2019-03-04 18:22:50 +0000

    test

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -15,6 +15,7 @@ class ReleaseController extends Controller
 
     public function store(Request $request)
     {
+        
         $name = $request->all()['pusher']['name'];
         $content = var_export($request->all(), true);
 
