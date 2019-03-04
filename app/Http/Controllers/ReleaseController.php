ea85a1071c46ecfcb03ddea165f45cdb5887f5b8
Bruno Barros <brunobarros2014@gmail.com>
2019-03-04 16:12:54 +0000

    test

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Release;
 use Illuminate\Http\Request;
+use mikehaertl\shellcommand\Command;
 
 class ReleaseController extends Controller
 {
@@ -14,8 +15,16 @@ class ReleaseController extends Controller
 
     public function store(Request $request)
     {
+
+        $command = new Command('git pull');
+        if ($command->execute()) {
+            $content = $command->getOutput();
+        } else {
+            $content = $command->getError();
+        }
+
         $name = "Bruno";
-        $content = shell_exec('pwd');
+        // $content = shell_exec('pwd');
         // $name = "1 - " . $name;
         // $content = var_export($request->all(), true);
         Release::create(['payload' => $content, 'name' => $name]);
