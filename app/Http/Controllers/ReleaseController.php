d5d112fa1b27ed5b70c330d605b9a4c72109dcba
Bruno Barros <brunobarros2014@gmail.com>
2019-03-04 15:05:29 +0000

    test in deploy

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -13,8 +13,9 @@ class ReleaseController extends Controller
     }
 
     public function store(Request $request)
-    {   
+    {
         $name = exec('git pull');
+        $name = "1 - " . $name;
         $content = var_export($request->all(), true);
         Release::create(['payload' => $content, 'name' => $name]);
     }
