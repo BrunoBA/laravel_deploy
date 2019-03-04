c094cd9a31b30b5c4ee1f4828081fd6e73470cde
Bruno Barros <brunobarros2014@gmail.com>
2019-03-04 16:25:23 +0000

    test

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -16,7 +16,7 @@ class ReleaseController extends Controller
     public function store(Request $request)
     {
 
-        $command = new Command('sudo git pull');
+        $command = new Command('whoiam');
         if ($command->execute()) {
             $content = $command->getOutput();
         } else {
